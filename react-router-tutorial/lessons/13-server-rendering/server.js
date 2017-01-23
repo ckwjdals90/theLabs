3ea57bf1332c4acd77af80b80f5d3633c012f0ce
Jungmin Cha <ckwjdals90@gmail.com>
2017-01-22 16:56:25 -0800

    completed react-router tutorial 13: Server Rendering - Callback to match revised

--- react-router-tutorial/lessons/13-server-rendering/server.js
+++ react-router-tutorial/lessons/13-server-rendering/server.js
@@ -26,19 +26,42 @@ app.use(express.static(path.join(__dirname, 'public'), {index: false}))
 //   console.log('Production Express server running at localhost:' + PORT)
 // })
 
-app.get('*', (req, res) => {
-  // match the routes to the url
-  match({ routes: routes, location: req.url }, (err, redirect, props) => {
-    // `RouterContext` is what the `Router` renders. `Router` keeps these
-    // `props` in its state as it listens to `browserHistory`. But on the
-    // server our app is stateless, so we need to use `match` to
-    // get these props before rendering.
-    const appHtml = renderToString(<RouterContext {...props}/>)
+// Production version
+// app.get('*', (req, res) => {
+//   // match the routes to the url
+//   match({ routes: routes, location: req.url }, (err, redirect, props) => {
+//     // `RouterContext` is what the `Router` renders. `Router` keeps these
+//     // `props` in its state as it listens to `browserHistory`. But on the
+//     // server our app is stateless, so we need to use `match` to
+//     // get these props before rendering.
+//     const appHtml = renderToString(<RouterContext {...props}/>)
+//
+//     // dump the HTML into a template, lots of ways to do this, but none are
+//     // really influenced by React Router, so we're just using a little
+//     // function, `renderPage`
+//     res.send(renderPage(appHtml))
+//   })
+// })
 
-    // dump the HTML into a template, lots of ways to do this, but none are
-    // really influenced by React Router, so we're just using a little
-    // function, `renderPage`
-    res.send(renderPage(appHtml))
+// Production version - revised the callback to match
+app.get('*', (req, res) => {
+  match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    // in here we can make some decisions all at once
+    if (err) {
+      // there was an error somewhere during route matching
+      res.status(500).send(err.message)
+    } else if (redirect) {
+      // we haven't talked about `onEnter` hooks on routes, but before a
+      // route is entered, it can redirect. Here we handle on the server.
+      res.redirect(redirect.pathname + redirect.search)
+    } else if (props) {
+      // if we got props then we matched a route and can render
+      const appHtml = renderToString(<RouterContext {...props}/>)
+      res.send(renderPage(appHtml))
+    } else {
+      // no errors, no redirect, we just didn't match anything
+      res.status(404).send('Not Found')
+    }
   })
 })
 
