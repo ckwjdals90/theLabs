c8c85b27510b735d26b2281370c331a52fc53618
Jungmin Cha <ckwjdals90@gmail.com>
2017-02-09 17:01:42 -0800

    added the nested element 'h2', javascript expression 'html' and a button with 'sayHey' method, and and inline style

--- velopert-react/src/components/App.js
+++ velopert-react/src/components/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+  sayHey() {
+    alert("Hey!");
+  }
+
   render() {
+    let text = "Dev-Server"
+
+    let pStyle = {
+      color: 'aqua',
+      background: 'black'
+    }
+
     return (
       <div>
         <h1>Hello React Skeleton</h1>
-        <h2>Welcome</h2>
+        <h2>Welcome to {text}</h2>
+        <button onClick={this.sayHey}>Click Me</button>
+        <p style = {pStyle}>{1 == 1 ? 'True' : 'False'}</p>
       </div>
     );
   }
