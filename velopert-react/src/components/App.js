346fed0f587cf637f6939c1123996828d9ef8bfe
Jungmin Cha <ckwjdals90@gmail.com>
2017-02-09 16:52:11 -0800

    installed dependencies and dev-dependencies, configured the skeleton structures, and created the basic templates

---
+++ velopert-react/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello React Skeleton</h1>
+        <h2>Welcome</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
