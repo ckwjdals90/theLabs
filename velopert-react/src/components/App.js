455636e798ec50eeeb36ab0fa2f9ec530f5104d7
Jungmin Cha <ckwjdals90@gmail.com>
2017-02-09 17:26:02 -0800

    States and Props - setting default props by using className.defaultProps

--- velopert-react/src/components/App.js
+++ velopert-react/src/components/App.js
@@ -1,17 +1,23 @@
 import React, {Component} from 'react';
-
-import Header from './Header.js'
-import Content from './Content.js'
+import ReactDOM from 'react-dom';
+import Header from './Header';
+import Content from './Content';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <Content />
+        <Header title={this.props.headerTitle} />
+        <Content title={this.props.contentTitle} body={this.props.contentBody} />
       </div>
     );
   }
 }
+
+App.defaultProps = {
+    headerTitle: 'Default header',
+    contentTitle: 'Default contentTitle',
+    contentBody: 'Default contentBody'
+};
 
 export default App;
