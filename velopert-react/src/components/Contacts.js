a9f4677ae0ba369e425b0d86e9ba4e4b1c0fbada
Jungmin Cha <ckwjdals90@gmail.com>
2017-02-18 15:59:42 -0800

    Immutable Helper - how to push new data to the existing array in the state - fixed(import 'update')

--- velopert-react/src/components/Contacts.js
+++ velopert-react/src/components/Contacts.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import update from 'react-addons-update';
 import ContactInfo from './ContactInfo';
 import ContactCreator from './ContactCreator';
 
