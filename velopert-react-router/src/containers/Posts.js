38d9e75b8b70d1a8f45e769418405015d96b5105
Jungmin Cha <ckwjdals90@gmail.com>
2017-02-23 01:52:30 -0800

    velopert-react-router tutorial - creating PostLinks component which renders the list of posts in Posts component

--- velopert-react-router/src/containers/Posts.js
+++ velopert-react-router/src/containers/Posts.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BigText from '../components/BigText';
+import PostLinks from '../components/PostLinks';
 
 const Posts = ({children}) => {
   return (
     <div>
       <BigText>Posts</BigText>
+      <PostLinks />
       {children}
     </div>
   );
