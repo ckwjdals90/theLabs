baf6e3459d41f8e87910fcd8425a99604f583623
Jungmin Cha <ckwjdals90@gmail.com>
2017-02-23 01:37:38 -0800

    velopert-react-router tutorial - applying active component css effects by using active attribute object

--- velopert-react-router/src/components/Header.js
+++ velopert-react-router/src/components/Header.js
@@ -3,24 +3,29 @@ import { Link } from 'react-router';
 import './Header.css';
 
 const MenuItem = ({active, children, to}) => (
-  <Link to={to} className="menu-item">
+  <Link to={to} className={`menu-item ${active ? 'active' : ''}`}>
     {children}
   </Link>
 );
 
-const Header = () => {
+const Header = (props, context) => {
+  const { router } = context;
   return (
     <div>
       <div className="logo">
         velopert
       </div>
       <div className="menu">
-        <MenuItem to={'/'}>Home</MenuItem>
-        <MenuItem to={'/about'}>About</MenuItem>
-        <MenuItem to={'/posts'}>Posts</MenuItem>
+        <MenuItem to={'/'} active={router.isActive('/', true)}>Home</MenuItem>
+        <MenuItem to={'/about'} active={router.isActive('/about')}>About</MenuItem>
+        <MenuItem to={'/posts'} active={router.isActive('/posts')}>Posts</MenuItem>
       </div>
     </div>
   );
 };
+
+Header.contextTypes = {
+  router: React.PropTypes.object
+}
 
 export default Header;
